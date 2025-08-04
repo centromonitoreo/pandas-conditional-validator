dd9fa28cb977f626c36540ece482aab94ee5c833
SergioRamirez94 <116461466+SergioRamirez94@users.noreply.github.com>
2025-08-04 16:08:45 -0500

    Add demo data and rule configuration

--- demo.py
+++ demo.py
@@ -1,12 +1,12 @@
 from df_rule_validator.services import ValidationService
 from df_rule_validator.rules_schema import RulesConfig
 import pandas as pd
-# Cargar reglas
-rules = RulesConfig.from_yaml("examples/rules_parametric.yaml")
 
-df = pd.read_csv("examples/data_example.csv")
+# Cargar reglas y datos de ejemplo
+rules = RulesConfig.from_yaml("examples/rules.yml")
+df = pd.read_csv("examples/demo_data.csv")
 
-varsalidation_service = ValidationService()
-failed_rows, result_df = varsalidation_service.validate(
-    df,
-    rules)
+validation_service = ValidationService()
+results = validation_service.validate(df, rules)
+
+print(results)
